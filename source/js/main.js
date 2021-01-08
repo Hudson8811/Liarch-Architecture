4887246a6f5aa74601d6fd7a34706ba1fbc24748
ArtZ <snickgem@gmail.com>
2021-01-08 10:21:47 +0400

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -564,10 +564,12 @@ function setOverlay(cb) {
 		},
 	});
 
-	$('.masonry').isotope({
-		itemSelector: '.masonry-item',
-		layoutMode: 'packery'
-	});
+	setTimeout(function () {
+		$('.masonry').isotope({
+			itemSelector: '.masonry-item',
+			layoutMode: 'packery'
+		});
+	}, 100);
 
 	select.on('change', function () {
 		var value = select.val();
